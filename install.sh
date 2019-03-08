1adf727df12619dd2a0055fe1447fca7d7a77fa7
StuartIanNaylor <stuartiannaylor@outlook.com>
2019-03-08 07:48:25 +0000

    First push

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+systemctl -q is-active zram-swap-config  && { echo "ERROR: zram-swap-config service is still running. Please run \"sudo service zram-swap-config stop\" to stop it."; exit 1; }
+[ "$(id -u)" -eq 0 ] || { echo "You need to be ROOT (sudo can be used)"; exit 1; }
+
+# zram-swap-config
+mkdir -p /usr/local/bin/
+install -m 644 zram-swap-config.service /etc/systemd/system/zram-swap-config.service
+install -m 755 zram-swap-config /usr/local/bin/zram-swap-config
+install -m 644 zram-swap-config.conf /etc/zram-swap-config.conf
+install -m 644 uninstall.sh /usr/local/bin/zram-swap-config-uninstall.sh
+systemctl enable zram-swap-config
+
+
+echo "##### Reboot to activate zram-swap-config #####"
---
+++ uninstall.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+if [ "$(id -u)" -eq 0 ]
+then
+  service zram-swap-config stop
+  systemctl disable zram-swap-config
+  rm /etc/systemd/system/zram-swap-config.service
+  rm /usr/local/bin/zram-swap-config
+  rm /etc/zram-swap-config.conf
+  
+  echo "zram-swap-config is uninstalled, removing the uninstaller in progress"
+  rm /usr/local/bin/zram-swap-config-uninstall.sh
+  echo "##### Reboot isn't needed #####"
+else
+  echo "You need to be ROOT (sudo can be used)"
+fi
