d058ac5a3d692b75d7588e755a4d482a70da71c6
StuartIanNaylor <stuartiannaylor@outlook.com>
2019-04-20 14:15:45 +0000

    change to systemctl

--- uninstall.sh
+++ uninstall.sh
@@ -2,12 +2,10 @@
 
 if [ "$(id -u)" -eq 0 ]
 then
-  service zram-swap-config stop
+  systemctl stop zram-swap-config 
   systemctl disable zram-swap-config
   rm /etc/systemd/system/zram-swap-config.service
   rm /usr/local/bin/zram-swap-config
-  #rm /usr/local/bin/zram-swap-config-slb
-  #rm /usr/local/bin/zram-swap-config-slb-pid
   rm /etc/zram-swap-config.conf
   
   echo "zram-swap-config is uninstalled, removing the uninstaller in progress"
